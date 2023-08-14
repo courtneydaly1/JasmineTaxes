39d62318d6cd726ad7fa86137ccd1c10c3c11b8a
Courtney Daly <courtneydaly1@gmail.com>
2023-08-14 18:15:38 -0400

    calculator-test.js

--- jasmine-testing-exercises-starter/jasmine-testing-exercises-starter/calculator/calculator-test.js
+++ jasmine-testing-exercises-starter/jasmine-testing-exercises-starter/calculator/calculator-test.js
@@ -1,11 +1,33 @@
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
+describe('Calculate Monthly Rates Function', function() {
+
+  it('should calculate the monthly rate correctly', function () {
+  const values = {
+    amount: 10000,
+    years: 8,
+    rate: 5.8
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('130.44');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {
+    amount: 125000,
+    years: 5,
+    rate: 3.2
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('2257.21');
+});
+
+it('should handle extremely low interest rates', function (){
+  const values = {
+    amount: 100,
+    years: 5.1,
+    rate: 0.25
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('1.65');
+  });
 });
 
 /// etc
